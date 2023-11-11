73459f81731814ee7546d8d34c3ac8826f97467a
Kenta Uehara <uehara@Kens-MacBook-Pro.local>
2023-11-11 20:56:57 +0200

    convert into function

--- rag.py
+++ rag.py
@@ -3,20 +3,22 @@ from langchain.embeddings import OpenAIEmbeddings
 
 from dotenv import load_dotenv
 
-load_dotenv()
+def run(query:str):
+    load_dotenv()
 
-query = "recent efficient method to produce stainless"
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.load_local("faiss_index", embeddings)
+    docs_and_scores = db.similarity_search_with_score(query)
 
-embeddings = OpenAIEmbeddings()
-db = FAISS.load_local("faiss_index", embeddings)
-# retriever = db.as_retriever()
-# docs = retriever.get_relevant_documents(query)
-docs_and_scores = db.similarity_search_with_score(query)
+    for d in docs_and_scores:
+        print(d[0].metadata)
+        print(f"score: {d[1]}")
+        print(d[0].page_content)
+        print("\n\n")
 
-for d in docs_and_scores:
-    print(d[0].metadata)
-    print(f"score: {d[1]}")
-    print(d[0].page_content)
-    print("\n\n")
+    return docs_and_scores
 
 
+if __name__ == "__main__":
+    query = "recent efficient method to produce stainless"
+    run(query)
