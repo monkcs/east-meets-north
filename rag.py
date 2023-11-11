60875cc52bd8c1e3f0572a9276c590c8b90107d1
Kenta Uehara <uehara@Kens-MacBook-Pro.local>
2023-11-11 20:11:01 +0200

    add similarity score

--- rag.py
+++ rag.py
@@ -5,12 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-query = "what is the efficient llm?"
+query = "recent efficient method to produce stainless"
 
 embeddings = OpenAIEmbeddings()
 db = FAISS.load_local("faiss_index", embeddings)
-retriever = db.as_retriever()
-docs = retriever.get_relevant_documents(query)
-print(docs)
+# retriever = db.as_retriever()
+# docs = retriever.get_relevant_documents(query)
+docs_and_scores = db.similarity_search_with_score(query)
+
+for d in docs_and_scores:
+    print(d[0].metadata)
+    print(f"score: {d[1]}")
+    print(d[0].page_content)
+    print("\n\n")
 
 
